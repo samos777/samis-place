c681f7f4a6a9ff84a5d7d7bd4e1dc512e07ab0d5
Shmuel Fellous <shmuel.f@compie.co.il>
2018-06-06 14:18:50 +0300

    more changes on slider and body still not the right size

--- styles/js/slider.js
+++ styles/js/slider.js
@@ -3,10 +3,44 @@
  */
 
 function initSlider(){
-    $('.thing').slick({
+    $('.mySlider').slick({
         dots:true,
         arrow:false,
         autoplay:true,
-        autoplaySpeed:1500
+        autoplaySpeed:1500,
+        infinite: true,
+        fade: true,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+
+
+
     })
+
+
+
 }
